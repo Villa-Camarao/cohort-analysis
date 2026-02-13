f9ee4d4febae398a2bb4c15be7d697e1102c8860
Emerson Santana <167444891+emerson-santana-vc@users.noreply.github.com>
2026-02-13 18:46:31 -0300

    Add configuration settings for AWS and redirect URI

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+AWS_BUCKET = "datalake-out-etl"
+FILE_KEY = "base-cohort-analysis/base-cohort-analysis.csv"
+REDIRECT_URI = "https://cohort-analysis-production.up.railway.app/_st_login"
+ALLOWED_DOMAIN = "villacamarao.com.br"
